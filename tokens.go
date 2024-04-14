099ed19f04cab385d2ebc45b64d25033f986efa7
Cale Black <terrorbyte@users.noreply.github.com>
2024-04-14 10:33:15 -0600

    Add Token Policy handling
    This patchset adds the policy creation and handling code as
    defined in:
    
    - https://desec.readthedocs.io/en/latest/auth/tokens.html#token-policy-management
    
    This allows for users to manage the token policy files in-library in
    order to add restrictions to individual tokens. An example use case
    would be creating tokens in CI and building a policy around them to
    restrict those tokens to only be able to change specific records.
    
    Adds the following functions:
    
    - `GetPolicies` - Gets all token policies.
    - `CreatePolicy` - Creates a new token policy.
    - `DeletePolicy` - Deletes a token policy.

--- tokens.go
+++ tokens.go
@@ -15,6 +15,15 @@ type Token struct {
 	Created *time.Time `json:"created,omitempty"`
 }
 
+// TokenPolicy represents a policy applied to a token.
+type TokenPolicy struct {
+	ID              string  `json:"id,omitempty"`
+	Domain          *string `json:"domain"`
+	SubName         *string `json:"subname"`
+	Type            *string `json:"type"`
+	WritePermission bool    `json:"perm_write,omitempty"` // Go `encoding/json` default boolean value is false
+}
+
 // TokensService handles communication with the tokens related methods of the deSEC API.
 //
 // https://desec.readthedocs.io/en/latest/auth/tokens.html
@@ -112,5 +121,97 @@ func (s *TokensService) Delete(ctx context.Context, tokenID string) error {
 		return handleError(resp)
 	}
 
+	return nil
+}
+
+// GetPolicies retrieves token rrset's policies.
+// https://desec.readthedocs.io/en/latest/auth/tokens.html#token-policy-management
+func (s *TokensService) GetPolicies(ctx context.Context, tokenID string) ([]TokenPolicy, error) {
+	endpoint, err := s.client.createEndpoint("auth", "tokens", tokenID, "policies", "rrsets")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create endpoint: %w", err)
+	}
+
+	req, err := s.client.newRequest(ctx, http.MethodGet, endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.client.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to call API: %w", err)
+	}
+
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, handleError(resp)
+	}
+
+	var policies []TokenPolicy
+	err = handleResponse(resp, &policies)
+	if err != nil {
+		return nil, err
+	}
+
+	return policies, nil
+}
+
+// CreatePolicy creates token policy.
+// https://desec.readthedocs.io/en/latest/auth/tokens.html#create-additional-tokens
+func (s *TokensService) CreatePolicy(ctx context.Context, tokenID string, policy TokenPolicy) (*TokenPolicy, error) {
+	endpoint, err := s.client.createEndpoint("auth", "tokens", tokenID, "policies", "rrsets")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create endpoint: %w", err)
+	}
+
+	req, err := s.client.newRequest(ctx, http.MethodPost, endpoint, policy)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.client.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to call API: %w", err)
+	}
+
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusCreated {
+		return nil, handleError(resp)
+	}
+
+	var tokenPolicy TokenPolicy
+	err = handleResponse(resp, &tokenPolicy)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tokenPolicy, nil
+}
+
+// DeletePolicy deletes a token rrset's policy.
+// https://desec.readthedocs.io/en/latest/auth/tokens.html#token-policy-management
+func (s *TokensService) DeletePolicy(ctx context.Context, tokenID, policyID string) error {
+	endpoint, err := s.client.createEndpoint("auth", "tokens", tokenID, "policies", "rrsets", policyID)
+	if err != nil {
+		return fmt.Errorf("failed to create endpoint: %w", err)
+	}
+
+	req, err := s.client.newRequest(ctx, http.MethodDelete, endpoint, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := s.client.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to call API: %w", err)
+	}
+
+	defer func() { _ = resp.Body.Close() }()
+
+	if resp.StatusCode != http.StatusNoContent {
+		return handleError(resp)
+	}
 	return nil
 }
--- tokens_test.go
+++ tokens_test.go
@@ -119,3 +119,162 @@ func TestTokensService_Delete(t *testing.T) {
 	err := client.Tokens.Delete(context.Background(), "aaa")
 	require.NoError(t, err)
 }
+
+func TestTokensService_GetPolicies(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	t.Cleanup(server.Close)
+
+	client := New("token", NewDefaultClientOptions())
+	client.BaseURL = server.URL
+
+	mux.HandleFunc("/auth/tokens/aaa/policies/rrsets/", func(rw http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodGet {
+			http.Error(rw, "invalid method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		file, err := os.Open("./fixtures/tokens_policy_get.json")
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer func() { _ = file.Close() }()
+
+		_, err = io.Copy(rw, file)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	tokens, err := client.Tokens.GetPolicies(context.Background(), "aaa")
+	require.NoError(t, err)
+
+	exampleDomain := "example.com"
+	exampleSubName := "testing"
+	exampleAType := "A"
+	expected := []TokenPolicy{
+		{
+			ID:              "7aed3f71-bc81-4f7e-90ae-8f0df0d1c211",
+			Domain:          &exampleDomain,
+			SubName:         &exampleSubName,
+			WritePermission: false,
+		},
+		{
+			ID:              "fa6fdf60-6546-4cee-9168-5d144fe9339c",
+			Domain:          &exampleDomain,
+			SubName:         &exampleSubName,
+			Type:            &exampleAType,
+			WritePermission: true,
+		},
+	}
+	assert.Equal(t, expected, tokens)
+}
+
+// This test is of the expected default all null JSON policy (ie creating a default deny), it'll get used often, so a separate test will help make sure it is accounted for.
+func TestTokensService_CreateEmptyPolicy(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	t.Cleanup(server.Close)
+
+	client := New("token", NewDefaultClientOptions())
+	client.BaseURL = server.URL
+
+	mux.HandleFunc("/auth/tokens/aaa/policies/rrsets/", func(rw http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodPost {
+			http.Error(rw, "invalid method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		rw.WriteHeader(http.StatusCreated)
+		file, err := os.Open("./fixtures/tokens_policy_create_empty.json")
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer func() { _ = file.Close() }()
+
+		_, err = io.Copy(rw, file)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	tokens, err := client.Tokens.CreatePolicy(context.Background(), "aaa", TokenPolicy{})
+	require.NoError(t, err)
+
+	expected := &TokenPolicy{
+		ID:              "a563a574-33c9-45d1-9201-e5577b42aaf1",
+		WritePermission: false,
+	}
+	assert.Equal(t, expected, tokens)
+}
+
+func TestTokensService_CreatePolicy(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	t.Cleanup(server.Close)
+
+	client := New("token", NewDefaultClientOptions())
+	client.BaseURL = server.URL
+
+	mux.HandleFunc("/auth/tokens/aaa/policies/rrsets/", func(rw http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodPost {
+			http.Error(rw, "invalid method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		rw.WriteHeader(http.StatusCreated)
+		file, err := os.Open("./fixtures/tokens_policy_create.json")
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer func() { _ = file.Close() }()
+
+		_, err = io.Copy(rw, file)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	exampleDomain := "example.com"
+	exampleSubName := "testing"
+	exampleAType := "A"
+
+	tokens, err := client.Tokens.CreatePolicy(context.Background(), "aaa", TokenPolicy{Domain: &exampleDomain, SubName: &exampleSubName, Type: &exampleAType, WritePermission: true})
+	require.NoError(t, err)
+
+	expected := &TokenPolicy{
+		ID:              "2f133e8e-56a0-4b19-8e7e-f2e29c7ce263",
+		Domain:          &exampleDomain,
+		SubName:         &exampleSubName,
+		Type:            &exampleAType,
+		WritePermission: true,
+	}
+	assert.Equal(t, expected, tokens)
+}
+
+func TestTokensService_DeletePolicy(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	t.Cleanup(server.Close)
+
+	client := New("token", NewDefaultClientOptions())
+	client.BaseURL = server.URL
+
+	mux.HandleFunc("/auth/tokens/aaa/policies/rrsets/bbb/", func(rw http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodDelete {
+			http.Error(rw, "invalid method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		rw.WriteHeader(http.StatusNoContent)
+	})
+
+	err := client.Tokens.DeletePolicy(context.Background(), "aaa", "bbb")
+	require.NoError(t, err)
+}
